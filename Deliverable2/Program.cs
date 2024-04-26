ffc82686b3e6c40488eca9d4ba9f91db65e1a1da
Zuhair Ali <szali172@gmail.com>
2024-04-26 10:02:21 -0500

    Added coward message

---
+++ Deliverable2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Initialize username
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+
+// They opted out
+Console.WriteLine("Do you want to do the COIN FLIP CHALLENGE?");
+if (Console.ReadLine() == "No")
+{
+    Console.WriteLine("Coward...");
+}
+
+// They're starting the challenge
+else
+{
+
+}
